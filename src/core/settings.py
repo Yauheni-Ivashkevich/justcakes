f5b8a81661d453ad23a40ad3ff70c34db5f28dbf
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-07-29 23:33:27 +0300

    configured allowed hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f-*^44+htjxz(qxbe=)&)=@tg5$0fr3-m+l$%&lnakz3u_fwxj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "justcakes.herokuapp.com"]
 
 
 # Application definition
