6ade3254ffb28e164434943b2c5a27e8dda10a96
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-07-30 00:47:38 +0300

    applications

---
+++ src/applications/index/admin.py
@@ -0,0 +1,15 @@
+# from django.contrib import admin
+# from django.contrib.admin import ModelAdmin
+#
+# from apps.index.models import MainPage
+# from apps.index.models import UserInfo
+#
+#
+# @admin.register(UserInfo)
+# class UserInfoAdminModel(ModelAdmin):
+#     pass
+#
+#
+# @admin.register(MainPage)
+# class MainPageAdminModel(ModelAdmin):
+#     pass
--- src/core/urls.py
+++ src/core/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #---applications---
+    path("", include("applications.index.urls")),
+    path("range/", include("applications.range.urls")),
+    # path("payment/", include("applications.payment.urls")),
+    # path("contacts/", include("applications.contacts.urls")),
 ]
