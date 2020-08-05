958983f704d2b4d958466bed0aa9b79e35e26b7f
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-08-05 14:34:05 +0300

    Added app_index

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
 
 from dynaconf import settings as _settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CORE_DIR = Path(__file__).parent.resolve()
+BASE_DIR = CORE_DIR.parent.resolve()
+REPO_DIR = BASE_DIR.parent.resolve()
 
 
 # Quick-start development settings - unsuitable for production
@@ -49,7 +52,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [f"{BASE_DIR}/core/templates"],
+        'DIRS': [CORE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,4 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = [CORE_DIR / 'static']
+
+STATIC_ROOT = REPO_DIR / '.static'
