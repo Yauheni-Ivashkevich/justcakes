d59ea0aef5eacda37b3c3adb4aef8fc733d9192c
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-08-19 14:24:11 +0300

    Added app_index

--- src/applications/index/models.py
+++ src/applications/index/models.py
@@ -1,3 +1,14 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models as m
+#
+#
+# class Cake(m.Model):
+#     title = m.CharField(max_length=100)
+#     description = m.TextField()
+#     ingredients = m.CharField(max_length=100)
+#     image = m.FilePathField(path="/images/")
+#
+#     class Meta:
+#         verbose_name_plural = "Cake"
+#
+#     def __str__(self):
+#         return f"Cake [id_{self.pk}]"
---
+++ src/applications/range/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+#
+# from applications.range.apps import RangeConfig
+# from applications.range.views import RangeView
+#
+# app_name = RangeConfig.name
+#
+# urlpatterns = [
+#     path("", RangeView.as_view(), name="cake_detail"),
+# ]
