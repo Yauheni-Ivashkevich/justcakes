b82696f399d3e99fd27a7ddf40207ba5763e1b17
Yauheni-Ivashkevich <zheklex@ya.ru>
2020-08-19 16:32:49 +0300

    Added templates

--- src/applications/index/views.py
+++ src/applications/index/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index/cake_index.html')
+    return render(request, 'index/cake_index.html', {
+        'body_message':
+            ['Cakes', 'Cupcakes', 'Gingers'],
+    })
